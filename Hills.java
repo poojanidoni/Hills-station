14f03e630620cf18845e6a056aaa651e5aef9f59
poojanidoni <poojanidonixworksz@gmail.com>
2021-12-27 23:31:22 +0530

    Hill station

---
+++ Hills.java
@@ -0,0 +1,15 @@
+class Hills{
+ public static void main(String []riya){
+	System.out.print("main() started");
+ for (int index=0;index<riya.length;index++){
+	if(index%3==0){
+	System.out.println();
+	{
+	System.out.println("him");
+	}
+	System.out.print(riya[index]+"\t");
+ }
+	System.out.println("\n End of main()");
+ }
+ }
+}
